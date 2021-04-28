db1783f0c5aaeafe614e01dbe8f7b0221fbf19d8
Pantelis Tamtakos <tampantelis@gmail.com>
2021-04-28 16:46:29 +0200

    created the basic renderer keyboard and speaker function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Renderer from "./scripts/renderer";
+import Keyboard from "./scripts/keyboard";
+import Speaker from "./scripts/speaker";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.renderer = React.createRef();
+    this.keyboard = React.createRef();
+    this.speaker = React.createRef();
+    this.state = {
+      loop: "",
+      fps: 60,
+      fpsInterval: "",
+      startTime: "",
+      now: "",
+      then: "",
+      elapsed: "",
+    };
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  init = () => {
+    this.setState({
+      fpsInterval: 1000 / this.state.fps,
+      then: Date.now(),
+      startTime: Date.now(),
+    });
+
+    this.setState({
+      loop: requestAnimationFrame(this.step),
+    });
+  };
+
+  step = () => {
+    this.setState({
+      now: Date.now(),
+      elapsed: Date.now() - this.state.then,
+    });
+
+    if (this.state.elapsed > this.state.fpsInterval) {
+      // Cycle the CPU. We'll come back to this later and fill it out.
+    }
+
+    this.setState({
+      loop: requestAnimationFrame(this.step),
+    });
+  };
+
+  handleTest = () => {
+    this.renderer.current.testRender();
+    this.renderer.current.renderScreen();
+  };
+
   render = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Renderer ref={this.renderer} />
+        <Keyboard ref={this.keyboard} />
+        <Speaker ref={this.speaker} />
+        <button onClick={this.handleTest}>Test</button>
       </div>
     );
   };
